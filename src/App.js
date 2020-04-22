2e5b5747f9a09fdf3bd00ac3ab0e1ed7b02242ea
Przemek Gorecki <przemyslaw.gorecki93@gmail.com>
2020-04-22 10:01:10 +0200

    Implement Material Design

--- src/App.js
+++ src/App.js
@@ -5,20 +5,30 @@ import Login from './components/views/Login/Login';
 import Kitchen from './components/views/Kitchen/Kitchen';
 import Tables from './components/views/Tables/Tables';
 import Waiter from './components/views/Waiter/Waiter';
+import { StylesProvider } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#2B4C6F'},
+  },
+});
 
 function App() {
   return (
     <BrowserRouter basename={'/'}>
-      <MainLayout>
-
-        <Switch>
-          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
-          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
-          <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
-        </Switch>
-
-      </MainLayout>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <MainLayout>
+            <Switch>
+              <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+              <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+              <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+              <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
+            </Switch>
+          </MainLayout>
+        </ThemeProvider>
+      </StylesProvider>
     </BrowserRouter>
   );
 }
--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PageNav from '../PageNav/PageNav';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Container from '@material-ui/core/Container';
 
 const MainLayout = (props) => {
   return(
     <div>
-      <PageNav />
-      {props.children}
+      <AppBar>
+        <Container maxWidth='lg'>
+          <Toolbar disableGutters>
+            <PageNav />
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Container maxWidth='lg'>
+        <Toolbar />
+        {props.children}
+      </Container>
     </div>
   );
 };
